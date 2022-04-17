4f6ca8151fb6344fd7a0a29ffbf2dbf5617499b4
j0n3lson <620536+j0n3lson@users.noreply.github.com>
2022-04-17 15:20:14 -0700

    [JavaCalcApp] Factors out button handling code for testing.
    Now ButtonHandler.process() returns updated text.

---
+++ JavaCalcApp/app/src/main/java/com/example/javacalcapp/ButtonHandler.java
@@ -0,0 +1,106 @@
+package com.example.javacalcapp;
+
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ButtonHandler {
+    static final ArrayList<Integer> DIGIT_BUTTON_IDS = new ArrayList<>(
+            Arrays.asList(
+                    R.id.btnDigit00,
+                    R.id.btnDigit01,
+                    R.id.btnDigit02,
+                    R.id.btnDigit03,
+                    R.id.btnDigit04,
+                    R.id.btnDigit05,
+                    R.id.btnDigit06,
+                    R.id.btnDigit07,
+                    R.id.btnDigit08,
+                    R.id.btnDigit09
+            ));
+
+    /**
+     * Handles button clicks returning the corresponding updated text if appropriate.
+     *
+     * @return The updated text with the new character appended (if allowed).
+     */
+    static String process(Button viewButton, String currentText) {
+        // Digit buttons are all the same
+        if (DIGIT_BUTTON_IDS.contains(viewButton.getId())) {
+            if (viewButton.getId() == R.id.btnDigit00 && lastCharMatches(currentText, "/")) {
+                return currentText;
+            }
+            return currentText + viewButton.getText().toString();
+        }
+
+        // These are special buttons that need different handling.
+        switch (viewButton.getId()) {
+            case R.id.btnClear:
+                return "";
+            case R.id.btnDelete:
+                // Only clear the last character
+                if (currentText.length() > 0) {
+                    return currentText.substring(0, currentText.length() - 1);
+                }
+            case R.id.btnDot:
+                // Cannot add a dot if there's already a dot in the string.
+                if (textContains(currentText, '.')) {
+                    return currentText;
+                }
+                return currentText + ".";
+            case R.id.btnParens:
+                // TODO: Implement btnParens
+                return currentText;
+            case R.id.btnDivide:
+                if (textContains(currentText, '/')) {
+                    return currentText;
+                }
+                return currentText + "/";
+            case R.id.btnMultiply:
+                if (lastCharMatches(currentText, "\\*")) {
+                    break;
+                }
+                return currentText + "*";
+            case R.id.btnAdd:
+                if (lastCharMatches(currentText, "\\+")) {
+                    return currentText;
+                }
+                return currentText + "+";
+            case R.id.btnSubtract:
+                if (lastCharMatches(currentText, "\\-")) {
+                    return currentText;
+                }
+                return currentText + "-";
+            case R.id.btnEquals:
+                // TODO: Implement btnEquals using ANTLRv4 parsing.This is a good approach because
+                // (1) It supports context free grammars and can support balanced parens check
+                // (2) It's been a long long time since I messed with lexers/parsers
+                // (3) It's possible (likely) that the grammar could replace all this manual
+                //      code and let us have a more expressive rule set.
+                // See https://stackoverflow.com/a/7597623 for ANTLRv4 on Java
+                // See https://stackoverflow.com/a/9609118 for working example
+                // See https://github.com/antlr/grammars-v4/tree/master/calculator for a grammar.
+                break;
+        }
+        return currentText;
+    }
+
+    private static boolean textContains(String text, char matchChar) {
+        if (text.isEmpty() || matchChar == ' ') {
+            return false;
+        }
+        return text.contains(Character.toString(matchChar));
+    }
+
+    private static char getLastChar(String text) {
+        if (text.isEmpty()) {
+            return ' ';
+        }
+        return text.charAt(text.length() - 1);
+    }
+
+    private static boolean lastCharMatches(String text, String regex) {
+        return Character.toString(getLastChar(text)).matches(regex);
+    }
+}
--- JavaCalcApp/app/src/main/java/com/example/javacalcapp/MainActivity.java
+++ JavaCalcApp/app/src/main/java/com/example/javacalcapp/MainActivity.java
@@ -9,23 +9,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class MainActivity extends AppCompatActivity {
-    private static final ArrayList<Integer> DIGIT_BUTTON_IDS = new ArrayList<>(
-            Arrays.asList(
-                    R.id.btnDigit00,
-                    R.id.btnDigit01,
-                    R.id.btnDigit02,
-                    R.id.btnDigit03,
-                    R.id.btnDigit04,
-                    R.id.btnDigit05,
-                    R.id.btnDigit06,
-                    R.id.btnDigit07,
-                    R.id.btnDigit08,
-                    R.id.btnDigit09
-            ));
     TextView txtCalcArea;
 
     @Override
@@ -40,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < childViewCount; i++) {
             View v = rootConstraintLayout.getChildAt(i);
             if (v instanceof Button) {
-                v.setOnClickListener(this::doButtonClick);
+                v.setOnClickListener(this::handleButtonClick);
             }
         }
         this.txtCalcArea = findViewById(R.id.txtCalcArea);
@@ -52,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
      * @param view the {@link View} that was clicked.
      */
     @SuppressLint("SetTextI18n")
-    public void doButtonClick(View view) {
+    public void handleButtonClick(View view) {
         // TODO: Refactor this into a ButtonHandler class and unit test it. We can use
         // parameterized testing for this. See https://stackoverflow.com/a/46553713
         boolean isButtonView = view instanceof Button;
@@ -60,92 +44,9 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        String currentText = this.txtCalcArea.getText().toString();
         Button viewButton = (Button) view;
-
-        // Digit buttons are all the same
-        if (DIGIT_BUTTON_IDS.contains(viewButton.getId())) {
-            if (viewButton.getId() == R.id.btnDigit00 && lastCharMatches(currentText, "/")) {
-                return;
-            }
-            txtCalcArea.setText(currentText + viewButton.getText().toString());
-            return;
-        }
-
-        // These are special buttons that need different handling.
-        switch (viewButton.getId()) {
-            case R.id.btnClear:
-                txtCalcArea.setText("");
-                break;
-            case R.id.btnDelete:
-                // Only clear the last character
-                if (currentText.length() > 0) {
-                    txtCalcArea.setText(currentText.substring(0, currentText.length() - 1));
-                }
-                break;
-            case R.id.btnDot:
-                // Cannot add a dot if there's already a dot in the string.
-                if (textContains(currentText, '.')) {
-                    break;
-                }
-                txtCalcArea.setText(currentText + ".");
-                break;
-            case R.id.btnParens:
-                // TODO: Implement btnParens
-                break;
-            case R.id.btnDivide:
-                if (textContains(currentText, '/')) {
-                    break;
-                }
-                txtCalcArea.setText(currentText + "/");
-                break;
-            case R.id.btnMultiply:
-                if (lastCharMatches(currentText, "\\*")) {
-                    break;
-                }
-                txtCalcArea.setText(currentText + "*");
-                break;
-            case R.id.btnAdd:
-                if (lastCharMatches(currentText, "\\+")) {
-                    break;
-                }
-                txtCalcArea.setText(currentText + "+");
-                break;
-            case R.id.btnSubtract:
-                if (lastCharMatches(currentText, "\\-")) {
-                    break;
-                }
-                txtCalcArea.setText(currentText + "-");
-                break;
-            case R.id.btnEquals:
-                // TODO: Implement btnEquals using ANTLRv4 parsing.This is a good approach because
-                // (1) It supports context free grammars and can support balanced parens check
-                // (2) It's been a long long time since I messed with lexers/parsers
-                // (3) It's possible (likely) that the grammar could replace all this manual
-                //      code and let us have a more expressive rule set.
-                // See https://stackoverflow.com/a/7597623 for ANTLRv4 on Java
-                // See https://stackoverflow.com/a/9609118 for working example
-                // See https://github.com/antlr/grammars-v4/tree/master/calculator for a grammar.
-                break;
-        }
-    }
-
-
-    private boolean textContains(String text, char matchChar) {
-        if (text.isEmpty() || matchChar == ' ') {
-            return false;
-        }
-        return text.contains(Character.toString(matchChar));
-    }
-
-    private char getLastChar(String text) {
-        if (text.isEmpty()) {
-            return ' ';
-        }
-        return text.charAt(text.length() - 1);
-    }
-
-    private boolean lastCharMatches(String text, String regex) {
-        return Character.toString(getLastChar(text)).matches(regex);
+        String currentText = txtCalcArea.getText().toString();
+        String updatedText = ButtonHandler.process(viewButton, currentText);
+        txtCalcArea.setText(updatedText);
     }
 }
