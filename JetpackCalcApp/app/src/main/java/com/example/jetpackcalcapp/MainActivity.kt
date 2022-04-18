11f7cceac2b142eb5bf83c11a61fd33dc25d7b75
j0n3lson <620536+j0n3lson@users.noreply.github.com>
2022-04-17 19:27:32 -0700

    [JetpackCalcApp] Basic calc-grid

--- JetpackCalcApp/app/src/main/java/com/example/jetpackcalcapp/MainActivity.kt
+++ JetpackCalcApp/app/src/main/java/com/example/jetpackcalcapp/MainActivity.kt
@@ -3,41 +3,94 @@ package com.example.jetpackcalcapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcalcapp.ui.theme.JetpackCalcAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackCalcAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            JetPackCalcApp()
+        }
+    }
+}
+
+@Composable
+fun JetPackCalcApp() {
+    JetpackCalcAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Column() {
+                TxtCalcArea()
+                ColButtonArea()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TxtCalcArea() {
+    Text(text = "txtCalcArea")
+}
+
+@Composable
+fun ColButtonArea() {
+    Row() {
+        ButtonBasic("AC")
+        ButtonBasic("( )")
+        ButtonBasic("%")
+        ButtonBasic("/")
+    }
+    Row() {
+        ButtonBasic("7")
+        ButtonBasic("8")
+        ButtonBasic("9")
+        ButtonBasic("x")
+    }
+
+    Row() {
+        ButtonBasic("4")
+        ButtonBasic("5")
+        ButtonBasic("6")
+        ButtonBasic("-")
+    }
+    Row() {
+        ButtonBasic("1")
+        ButtonBasic("2")
+        ButtonBasic("3")
+        ButtonBasic("+")
+    }
+    Row() {
+        ButtonBasic("0")
+        ButtonBasic(".")
+        ButtonBasic("xx")
+        ButtonBasic("=")
+    }
+}
+
+@Composable
+fun ButtonBasic(value: String) {
+    Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(16.dp)) {
+        Text(value)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackCalcAppTheme {
-        Greeting("Android")
-    }
+    JetPackCalcApp()
 }
